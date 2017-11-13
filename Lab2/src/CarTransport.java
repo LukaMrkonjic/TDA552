ba82a8e51c2a85740d3c50e1507a4081f647d811
Luka Mrkonjic <lukamrkonjic163@gmail.com>
2017-11-13 17:29:49 +0100

    Adds a new Ferry class and changes methods in CarTransport

--- Lab2/src/CarTransport.java
+++ Lab2/src/CarTransport.java
@@ -3,26 +3,23 @@ import java.util.Stack;
 public class CarTransport extends Truck {
 
     LoadingPlatform loadingPlatform;
-    private int up = 90;
-    private int down = 0;
+    private final int up = 90;
+    private final int down = 0;
 
     public CarTransport() {
         Stack vehicles = new Stack();
         double maxStorageSpace = 100;
-        loadingPlatform.setAngle(up);
         loadingPlatform = new LoadingPlatform();
+        loadVehicles(loadingPlatform, up);
         VehicleStorage v = new VehicleStorage(maxStorageSpace, 50, vehicles);
     }
 
-    public void setPlatform(int i) {
-        if (i == up) {
-            loadingPlatform.setAngle(up);
-        }
-        else if (i == down && getCurrentSpeed() == 0) {
+    //TODO: Overload the setAngle method instead of creating a new one(this method)
+    public void loadVehicles(LoadingPlatform loadingPlatform, int angle) {
+        if (angle == 0 && getCurrentSpeed() == 0) {
             loadingPlatform.setAngle(down);
-        }
-        else {
-            System.out.println("Platform must be up or down.");
+        } else {
+            loadingPlatform.setAngle(angle);
         }
     }
 
