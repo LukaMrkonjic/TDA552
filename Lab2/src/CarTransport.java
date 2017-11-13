2591bac0957bc3d7e431cc3ffaee50a6fb142126
Luka Mrkonjic <lukamrkonjic163@gmail.com>
2017-11-13 16:26:27 +0100

    CarTransport is now finished

--- Lab2/src/CarTransport.java
+++ Lab2/src/CarTransport.java
@@ -1,3 +1,29 @@
+import java.util.Stack;
+
 public class CarTransport extends Truck {
+
+    LoadingPlatform loadingPlatform;
+    private int up = 90;
+    private int down = 0;
+
+    public CarTransport() {
+        Stack vehicles = new Stack();
+        double maxStorageSpace = 100;
+        loadingPlatform.setAngle(up);
+        loadingPlatform = new LoadingPlatform();
+        VehicleStorage v = new VehicleStorage(maxStorageSpace, 50, vehicles);
+    }
+
+    public void setPlatform(int i) {
+        if (i == up) {
+            loadingPlatform.setAngle(up);
+        }
+        else if (i == down && getCurrentSpeed() == 0) {
+            loadingPlatform.setAngle(down);
+        }
+        else {
+            System.out.println("Platform must be up or down.");
+        }
+    }
 
 }
